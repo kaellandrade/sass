de27a1f8e85d8128e21adb0b75a06ec66d9c384f
Micael andrade <micael.santos@dcomp.ufs.br>
2022-07-07 00:17:37 -0300

    Usando aninhamento e variáveis no sass

--- src/index.js
+++ src/index.js
@@ -7,14 +7,26 @@ function component() {
 	return element;
 }
 
-function renderBoxs(numberBox = 15) {
+function componentBtnTop() {
+	const btnTop = document.createElement('button');
+	btnTop.classList.add('btn-top');
+	btnTop.addEventListener('click', _ => window.scrollTo({top: 0, behavior: 'smooth'}))
+	return btnTop;
+}
+
+function renderBoxs(numberBox = 100) {
+	const zeroPad = (num, places) => String(num).padStart(places, '0');
+
 	const boxContainerElement = document.createElement('div');
 	boxContainerElement.classList.add('container');
 
 	const boxElementsList = [];
 	for (let i = 1; i <= numberBox; i++) {
 		const newBox = document.createElement('div');
-		newBox.innerHTML = `Dynamic box ${i}`;
+		const newTitle = document.createElement('h5');
+
+		newTitle.innerHTML = `Box number ${zeroPad(i, 2)}`;
+		newBox.appendChild(newTitle);
 		newBox.classList.add('box');
 		boxElementsList.concat(newBox);
 		boxContainerElement.appendChild(newBox);
@@ -23,4 +35,5 @@ function renderBoxs(numberBox = 15) {
 }
 
 document.body.appendChild(component());
+document.body.appendChild(componentBtnTop());
 renderBoxs();
